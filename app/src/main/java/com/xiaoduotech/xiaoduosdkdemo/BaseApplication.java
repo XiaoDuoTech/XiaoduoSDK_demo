bb3b00a9d3f99d8c4966453ce3228100ca2a3cdc
XiaoDuoTech <zhangchong@xiaoduotech.com>
2016-09-01 19:44:37 +0800

    Update BaseApplication.java

--- app/src/main/java/com/xiaoduotech/xiaoduosdkdemo/BaseApplication.java
+++ app/src/main/java/com/xiaoduotech/xiaoduosdkdemo/BaseApplication.java
@@ -13,8 +13,10 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        CVDManager.getInstance(this).init("16",11,"b5b1f884faf0ac251403435183ee5b140cb83ac28d62abab30cfe6145004ded4");
+        FIR.init(this);
         LeakCanary.install(this);
+        CVDManager.getInstance(this).init("16",11,"b5b1f884faf0ac251403435183ee5b140cb83ac28d62abab30cfe6145004ded4");
+       
 
     }
 }
